e809dd7d7e8a243075c942982c8f14bb27459218
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-12-02 10:21:20 +0300

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,15 +27,6 @@ module.exports = {
       borderRadius: {
         lg: "var(--radius)",
       },
-      keyframes: {
-        shimmer: {
-          '0%': { backgroundPosition: '-200% 0' },
-          '100%': { backgroundPosition: '200% 0' },
-        },
-      },
-      animation: {
-        shimmer: 'shimmer 1.5s infinite',
-      },
     },
   },
   plugins: [
